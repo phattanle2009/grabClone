9c3b36573e40eee6869108c7116f9e71bd63338b
Tan Phat Le <39773716+phattanle2009@users.noreply.github.com>
2023-04-13 14:40:15 +0700

    feat: add card collection section

---
+++ grab_clone/lib/home/sections/card_collection_section.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:grab_clone/constant/dimensions.dart';
+import 'package:grab_clone/constant/icon.dart';
+import 'package:grab_clone/home/sections/items/card_collection_item.dart';
+
+class CardCollectionSection extends StatelessWidget {
+  Widget _config() {
+    return Container(
+      height: 92,
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        scrollDirection: Axis.horizontal,
+        children: [
+          CardCollectionItem(title: "Activate", subTitle: "Moca", imageName: AppIcons.paypal),
+          AppDimensions.mediumWidthSpace,
+          CardCollectionItem(title: "Account", subTitle: "Verify Email", imageName: AppIcons.userColorful),
+          AppDimensions.mediumWidthSpace,
+          CardCollectionItem(title: "Point", subTitle: "0", imageName: AppIcons.dollar),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _config();
+  }
+}
---
+++ grab_clone/lib/home/sections/items/card_collection_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:grab_clone/constant/colors.dart';
+
+class CardCollectionItem extends StatelessWidget {
+  String title;
+  String subTitle;
+  String imageName;
+
+  CardCollectionItem({
+    required this.title,
+    required this.subTitle,
+    required this.imageName,
+  });
+
+  Widget _configItem() {
+    return AspectRatio(
+      aspectRatio: 2.5,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+          color: AppColors.lighterGrey,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Text(
+                title,
+                textAlign: TextAlign.left,
+                maxLines: 1,
+                style: const TextStyle(fontWeight: FontWeight.w300),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      subTitle,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Image.asset(
+                    imageName,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _configItem();
+  }
+}
