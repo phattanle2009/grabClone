6d0a8d0bc88657e2efde257c26cbf6b5e3bfbf34
Tan Phat Le <39773716+phattanle2009@users.noreply.github.com>
2023-04-13 21:01:05 +0700

    chore: add new section

--- grab_clone/lib/home/home_page.dart
+++ grab_clone/lib/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:grab_clone/common/mock.dart';
 import 'package:grab_clone/constant/colors.dart';
-import 'package:grab_clone/constant/dimensions.dart';
+import 'package:grab_clone/home/sections/card_collection_section.dart';
 import 'package:grab_clone/home/sections/collection_section.dart';
+import 'package:grab_clone/home/sections/order_now_section.dart';
+import 'package:grab_clone/home/sections/suggestion_section.dart';
 import 'package:grab_clone/home/sections/text_field.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,6 +25,16 @@ class _HomePageState extends State<HomePage> {
             children: [
               HomeTextFieldHeader(),
               CollectionSection(),
+              CardCollectionSection(),
+              OrderNowSection(),
+              SuggestionSection(
+                sectionHeader: "Restaurants you may like",
+                list: Mock.restaurants,
+              ),
+              SuggestionSection(
+                sectionHeader: "Food for you",
+                list: Mock.foods,
+              ),
             ],
           ),
         ),
