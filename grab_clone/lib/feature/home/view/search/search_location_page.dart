dc085293b58a5a59710643f92d5ad2147df7bce1
Tan Phat Le <39773716+phattanle2009@users.noreply.github.com>
2023-05-11 13:28:57 +0700

    feat: complete SearchLocation page

--- grab_clone/lib/feature/home/view/search/search_location_page.dart
+++ grab_clone/lib/feature/home/view/search/search_location_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grab_clone/common/mock.dart';
 import 'package:grab_clone/constant/dimensions.dart';
+import 'package:grab_clone/feature/home/view/search/search_list.dart';
 import 'package:grab_clone/feature/home/view/search/search_location_state.dart';
 import 'package:grab_clone/feature/home/view/search/search_location_page_bloc.dart';
 import 'package:grab_clone/feature/home/view/search/item/search_location_item.dart';
@@ -15,80 +16,109 @@ class SearchLocationPage extends StatefulWidget {
 
 class _SearchLocationPageState extends State<SearchLocationPage> {
   bool isSearchSuggestion = true;
-  ScrollController _scrollController = ScrollController();
+  ScrollController _listSearchScrollController = ScrollController();
+  ScrollController _listSuggestionScrollController = ScrollController();
   final _searchLocationPageBloc = SearchLocationPageBloc();
   var _searchLocationState = SearchLocationState();
 
   @override
   void initState() {
-    _scrollController.addListener(() {
-      _searchLocationPageBloc.changeOffset(_searchLocationState, _scrollController.offset);
-    });
+    _listSuggestionScrollController.addListener(
+      () {
+        _searchLocationPageBloc.changeOffset(
+          _searchLocationState,
+          _listSuggestionScrollController.offset,
+        );
+      },
+    );
+    _listSearchScrollController.addListener(
+      () {
+        _searchLocationPageBloc.changeOffset(
+          _searchLocationState,
+          _listSearchScrollController.offset,
+        );
+      },
+    );
     super.initState();
   }
 
   @override
   void dispose() {
-    _scrollController.dispose();
+    _listSearchScrollController.dispose();
+    _listSuggestionScrollController.dispose();
     _searchLocationPageBloc.dispose();
     super.dispose();
   }
 
-  Widget _buildAppBar(BuildContext context) {
-    var list = Mock.searchItems;
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
+  Widget _buildListItem(bool isSuggestionSearch) {
+    if (isSuggestionSearch) {
+      var list = Mock.searchItems;
+      return ListView.builder(
+        controller: _listSuggestionScrollController,
+        itemCount: list.length,
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppDimensions.mediumSize,
+        ),
+        scrollDirection: Axis.vertical,
+        itemBuilder: (BuildContext context, int index) {
+          return SearchLocationItem(
+            title: list[index].title,
+            address: list[index].detailAddress ?? "",
+            type: list[index].type,
+          );
+        },
+      );
+    } else {
+      return SearchList(scrollController: _listSearchScrollController);
+    }
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder(
+      stream: _searchLocationPageBloc.searchState,
+      builder: (context, snapshot) {
+        final data = snapshot.data ?? SearchLocationState();
+        return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            StreamBuilder(
-              stream: _searchLocationPageBloc.searchState,
-              builder: (context, snapshot) {
-                final data = snapshot.data ?? SearchLocationState();
-                return SearchLocationNavigation(
-                  navigationTitle: data.searchNavigationTitle,
-                  isSearchSuggestion: data.isSuggestionSearch,
-                  isShowShadow: data.isShowShadow,
-                  selectedLabel: data.selectedLabel,
-                  searchController: _searchLocationPageBloc.searchController,
-                  onCleared: () {
-                    _searchLocationPageBloc.searchController.clear();
-                    _searchLocationPageBloc.changeSuggestionSearch(_searchLocationState, true);
-                  },
-                  onSubmitted: (value) => _searchLocationPageBloc.changeSuggestionSearch(
-                    _searchLocationState,
-                    value == "",
-                  ),
-                  onTappedLabel: (value) {
-                    _searchLocationPageBloc.selectedLabel(_searchLocationState, value);
-                  },
+            SearchLocationNavigation(
+              navigationTitle: data.searchNavigationTitle,
+              isSearchSuggestion: data.isSuggestionSearch,
+              isShowShadow: data.isShowShadow,
+              isShowSearchBar: data.isShowSearchBar,
+              selectedLabel: data.selectedLabel,
+              searchController: _searchLocationPageBloc.searchController,
+              onCleared: () {
+                _searchLocationPageBloc.searchController.clear();
+                _searchLocationPageBloc.changeSuggestionSearch(
+                  _searchLocationState,
+                  true,
                 );
+              },
+              onSubmitted: (value) => _searchLocationPageBloc.changeSuggestionSearch(
+                _searchLocationState,
+                value == "",
+              ),
+              onTappedLabel: (value) {
+                _searchLocationPageBloc.selectedLabel(_searchLocationState, value);
               },
             ),
             Expanded(
-              child: ListView.builder(
-                controller: _scrollController,
-                itemCount: list.length,
-                padding: const EdgeInsets.symmetric(horizontal: AppDimensions.mediumSize),
-                scrollDirection: Axis.vertical,
-                itemBuilder: (BuildContext context, int index) {
-                  return SearchLocationItem(
-                    title: list[index].title,
-                    address: list[index].detailAddress ?? "",
-                    type: list[index].type,
-                  );
-                },
-              ),
+              child: _buildListItem(data.isSuggestionSearch),
             ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildAppBar(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: _buildBody(context),
+      ),
+    );
   }
 }
