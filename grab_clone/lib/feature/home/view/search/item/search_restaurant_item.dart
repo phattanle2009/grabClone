90a85b569d68dd1bb0d4487452ddcb2392c1b216
Tan Phat Le <39773716+phattanle2009@users.noreply.github.com>
2023-05-11 13:25:40 +0700

    feat: add search Restaurants section

---
+++ grab_clone/lib/feature/home/view/search/item/search_restaurant_item.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:grab_clone/constant/text.dart';
+import 'package:grab_clone/constant/icon.dart';
+import 'package:grab_clone/constant/colors.dart';
+import 'package:grab_clone/constant/dimensions.dart';
+
+class SearchRestaurantItem extends StatelessWidget {
+  String title;
+  String desc;
+  String imageName;
+  double distance;
+  double rating;
+  String routeTime;
+  String? moreInfos;
+  bool isPromo;
+
+  SearchRestaurantItem({
+    required this.title,
+    required this.desc,
+    required this.imageName,
+    required this.distance,
+    required this.rating,
+    required this.routeTime,
+    required this.moreInfos,
+    required this.isPromo,
+  });
+
+  Widget _buildThumbnail() {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(
+            AppDimensions.smallestSize,
+          ),
+          child: Image.asset(
+            imageName,
+            width: AppDimensions.avatarSize,
+            height: AppDimensions.avatarSize,
+            fit: BoxFit.fill,
+          ),
+        ),
+        _buildPromoItem(),
+      ],
+    );
+  }
+
+  Widget _buildPromoItem() {
+    return !isPromo
+        ? Container()
+        : Positioned.fill(
+            child: Wrap(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.orange,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        AppDimensions.smallestSize,
+                      ),
+                    ),
+                  ),
+                  padding: EdgeInsets.all(
+                    AppDimensions.smallestSize,
+                  ),
+                  child: Text(
+                    "Promo",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: AppDimensions.smallerFontSize,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+  }
+
+  Widget _buildTitle() {
+    return Text(
+      title,
+      style: AppTextStyles.smallMediumFont,
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  Widget _buildDesc() {
+    return Text(
+      desc,
+      style: TextStyle(
+        fontSize: AppDimensions.smallerFontSize,
+        color: AppColors.lightGray,
+      ),
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  Widget _buildInfo() {
+    return Row(
+      children: [
+        Image.asset(
+          AppIcons.star,
+          width: AppDimensions.imageSmallSize,
+          height: AppDimensions.imageSmallSize,
+        ),
+        AppDimensions.smallestWidthSpace,
+        Text(
+          "$rating",
+          style: AppTextStyles.smallerMediumFont,
+        ),
+        AppDimensions.smallerWidthSpace,
+        Image.asset(
+          AppIcons.clock,
+          width: AppDimensions.imageSmallSize,
+          height: AppDimensions.imageSmallSize,
+        ),
+        AppDimensions.smallestWidthSpace,
+        Text(
+          "$routeTime min · $distance km",
+          style: AppTextStyles.smallerMediumFont,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMoreInfos() {
+    if (moreInfos == null || moreInfos == "") {
+      return Text("");
+    }
+    return Row(
+      children: [
+        Image.asset(
+          AppIcons.spoonFork,
+          width: AppDimensions.imageSmallSize,
+          height: AppDimensions.imageSmallSize,
+        ),
+        AppDimensions.smallestWidthSpace,
+        Text(
+          moreInfos!,
+          style: AppTextStyles.smallerMediumFont,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildTitle(),
+        _buildDesc(),
+        AppDimensions.smallerHeightSpace,
+        _buildInfo(),
+        AppDimensions.smallestHeightSpace,
+        _buildMoreInfos(),
+      ],
+    );
+  }
+
+  Widget _buildingItem() {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: AppDimensions.smallSize,
+      ),
+      child: Row(
+        children: [
+          _buildThumbnail(),
+          AppDimensions.mediumWidthSpace,
+          Expanded(
+            child: _buildText(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildingItem();
+  }
+}
---
+++ grab_clone/lib/feature/home/view/search/item/search_restaurant_section.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:grab_clone/common/mock.dart';
+import 'package:grab_clone/constant/colors.dart';
+import 'package:grab_clone/constant/dimensions.dart';
+import 'package:grab_clone/feature/widgets/more_result.dart';
+import 'package:grab_clone/feature/widgets/header_title.dart';
+import 'package:grab_clone/feature/home/view/search/item/search_restaurant_item.dart';
+
+class SearchRestaurantSection extends StatelessWidget {
+  const SearchRestaurantSection({super.key});
+
+  Widget _buildRestaurants() {
+    var list = Mock.searchRestaurants;
+    return Wrap(
+      children: list.map(
+        (data) {
+          return SearchRestaurantItem(
+            title: data.merchantName,
+            desc: data.desc,
+            imageName: data.imageName,
+            distance: data.distance,
+            rating: data.rating,
+            routeTime: data.routeTime,
+            moreInfos: data.moreInfos,
+            isPromo: data.isPromo,
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  Widget _buildRestaurantsSection() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: AppDimensions.mediumSize,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppDimensions.largeHeightSpace,
+          HeaderTitle(sectionHeader: "Restaurants"),
+          AppDimensions.mediumHeightSpace,
+          _buildRestaurants(),
+          AppDimensions.mediumHeightSpace,
+          Container(
+            height: 1,
+            color: AppColors.lighterGrey,
+          ),
+          MoreResultButton(title: "More Restaurants"),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildRestaurantsSection();
+  }
+}
