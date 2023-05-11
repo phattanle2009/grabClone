1dcb41ca447f94968a1d500002297b9de9a9637a
Tan Phat Le <39773716+phattanle2009@users.noreply.github.com>
2023-05-11 13:27:32 +0700

    feat: add SearchList widget to Search Location page

---
+++ grab_clone/lib/feature/home/view/search/search_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:grab_clone/common/mock.dart';
+import 'package:grab_clone/constant/colors.dart';
+import 'package:grab_clone/constant/dimensions.dart';
+import 'package:grab_clone/feature/home/sections/suggestion_section.dart';
+import 'package:grab_clone/feature/home/view/search/item/search_destination_section.dart';
+import 'package:grab_clone/feature/home/view/search/item/search_restaurant_section.dart';
+import 'package:grab_clone/feature/widgets/more_result.dart';
+
+class SearchList extends StatelessWidget {
+  ScrollController scrollController;
+
+  SearchList({
+    required this.scrollController,
+  });
+
+  Widget _lightGreyColor() {
+    return Container(
+      height: AppDimensions.smallerSize,
+      color: AppColors.lighterGrey,
+    );
+  }
+
+  Widget _buildSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SuggestionSection(
+          sectionHeader: "Groceries and supplies",
+          list: Mock.foods,
+        ),
+        AppDimensions.mediumHeightSpace,
+        Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: AppDimensions.mediumSize,
+          ),
+          height: 1,
+          color: AppColors.lighterGrey,
+        ),
+        MoreResultButton(title: "More Groceries and supplies"),
+        _lightGreyColor(),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      controller: scrollController,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SearchDestinationSection(),
+          _lightGreyColor(),
+          SearchRestaurantSection(),
+          _lightGreyColor(),
+          _buildSection(),
+        ],
+      ),
+    );
+  }
+}
