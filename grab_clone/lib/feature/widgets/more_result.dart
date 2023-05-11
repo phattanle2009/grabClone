91632538b59b3aaaa57604ec4f42b89a9e50a882
Tan Phat Le <39773716+phattanle2009@users.noreply.github.com>
2023-05-11 13:26:39 +0700

    feat: add custom MoreButton widget

---
+++ grab_clone/lib/feature/widgets/more_result.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:grab_clone/constant/colors.dart';
+import 'package:grab_clone/constant/dimensions.dart';
+
+class MoreResultButton extends StatelessWidget {
+  String title;
+
+  MoreResultButton({
+    required this.title,
+  });
+
+  Widget _buildButton() {
+    return InkWell(
+      onTap: () {
+        print(title);
+      },
+      child: Container(
+        height: AppDimensions.customButtonHeight,
+        width: double.infinity,
+        child: Center(
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: AppColors.lighterBlue,
+              fontWeight: FontWeight.w500,
+              fontSize: AppDimensions.mediumFontSize,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildButton();
+  }
+}
